e2b017780ed5716a828103dfab20c68297e2b6a9
신채연 <tlscodusdla@gmail.com>
2025-11-27 11:00:08 +0900

    Add files via upload

---
+++ ball.py
@@ -0,0 +1,119 @@
+from tkinter import *
+import random
+import time
+
+class Ball:
+    def __init__(self, canvas,paddle, color):
+        self.canvas = canvas
+        self.paddle = paddle 
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color) 
+        self.canvas.move(self.id, 245, 100) #canvas.move(객체ID, X방향이동, Y방향이동)
+
+        starts = [-3,-2,-1,1,2,3]
+        random.shuffle (starts)
+        self.x = starts[0] #섞인 리스트의 첫번째 값을 사용
+
+        self.y = -3
+
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+
+        self.hit_bottom = False #바닥에 닿았는지 여부확인
+ 
+ # 공이랑 패들 만나게 하는 코드
+    def hit_paddle(self,pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+
+        if pos[2] >=paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3] :
+                return True
+        return False
+        
+        if self.hit_paddle(pos) == True :
+            self.y =-3
+
+        if pos[1] <=0 :
+            self.y = 3
+
+        if pos[3] >= self.canvas_height :
+            self.y =-3
+
+    def draw(self) :
+        self.canvas .move(self.id,self.x,self.y)
+        pos = self.canvas.coords(self.id)
+        print(self.canvas.coords(self.id))
+
+        if pos[1] <=0 :
+            self.y=3
+
+        if pos[3] >= self.canvas_height :
+            self.hit_bottom
+
+        if not self.hit_bottom :
+            if self.hit_paddle(pos) == True :
+                self.y = -3
+        if pos [0] <=0 :
+            self.x= 3
+
+        if pos[2] >= self.canvas_width :
+            self.x= -3
+    '''
+    def draw(self):
+        self.canvas.move(self.id, 0, -1) #매 프레임 공이 위로 1픽셀씩
+    '''
+
+
+#Paddle
+class Paddle :
+    def __init__(self,canvas,color) :
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color) 
+        self.canvas.move(self.id, 200, 300) #paddle 생성 위치
+
+        self.x = 0
+        self.canvas_width = self. canvas.winfo_width()
+    
+        self.canvas.bind_all ('<KeyPress-Left>',self.turn_left) #event 만들고 조작
+        self.canvas.bind_all ('<KeyPress-Right>',self.turn_right)
+
+    def turn_left(self,evt) :
+        self.x = -2
+
+    def turn_right(self,evt) :
+        self.x = 2
+
+    def draw(self) :
+        self.canvas.move(self.id,self.x,0)
+        pos =self.canvas.coords(self.id)
+
+        if pos[0] <=0 :
+            self.x = 0
+
+        elif pos[2] >= self.canvas_width :
+            self.x = 0
+
+
+#Tkinter
+tk = Tk()
+tk.title("Game")
+tk.resizable(0, 0)
+
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+
+rectangle = Paddle (canvas,'blue')
+ball = Ball(canvas,rectangle, 'red')
+while True :
+    if ball.hit_bottom == False:
+        ball.draw()
+        rectangle.draw()
+
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
+
+
+
+tk.mainloop()
